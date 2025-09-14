51051771b9bf4e1f69a5706a5e6706563886796f
Nagiyev Mahaddin <nmeheddin9@gmail.com>
2025-09-14 17:37:29 +0400

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.VITE_ENV),
+    },
+    server: {
+      port: 4800, // ✅ Set dev server port here
+      host: true,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    build: {
+      outDir: "dist",
+    },
+  };
+});
